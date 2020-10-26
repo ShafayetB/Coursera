ab7643637072ba2ff9d9d5d70e49804f3649ebc8
Shafayet Bhuiyan <shafayet.cse@gmail.com>
2020-10-26 14:38:58 +0600

    Add files via upload

---
+++ Accelerated-Computer-Science-Fundamentals/Object-Oriented Data Structures in C++/Assignments/Hello-World/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * University of Illinois
+ * Object-Oriented Data Structures in C++ - Week 2
+ * Hello World Project
+ *
+ * @author Eric Huber - University of Illinois staff - July 2019
+**/
+
+// You need to include some header(s) here!
+#include <iostream>
+
+// You need to define your main() function here!
+int main() {
+    std::cout << "Hello, world!\nGreetings from Illinois!" << std::endl;
+    return 0;
+}
+
+// Notes:
+
+// If you get a compiler error saying "undefined reference to main",
+// it means you haven't defined your main function correctly.
+
+// Standard output is the system stream for normal text output on the terminal.
+// One way to write to it, that you have learned, is with std::cout.
+
+// Your main function should write the following messages to standard output:
+// Hello, world!
+// Greetings from Illinois!
+
+// Also note, the autograder does not care about letter case, punctuation,
+// or spacing! But it does care about spelling and the order of words!
